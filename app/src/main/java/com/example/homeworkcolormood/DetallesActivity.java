5f6e96e5ad91c8817a57cf5b715c89493aa00f21
KnightAzuul <nadiaxmc@gmail.com>
2025-09-08 14:39:01 -0300

    Creacion de activity_detalles.xml y cambio de fondo

---
+++ app/src/main/java/com/example/homeworkcolormood/DetallesActivity.java
@@ -0,0 +1,15 @@
+package com.example.homeworkcolormood;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class DetallesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detalles);
+
+    }
+}
